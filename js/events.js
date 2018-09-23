56769bb4dfa1b7ac102bc3681a629c8a2cbfecc0
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-09-23 17:57:22 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt () {
 
 
 function pressIt () {
-  $('typing').keydown(function (key) {
+  $('input').keydown(function (key) {
   if (key.which == 71) {
     alert ("G was pressed");
   }
