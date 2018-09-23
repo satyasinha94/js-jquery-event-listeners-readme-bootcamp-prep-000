f05f0f59e3a228782aff39f96c56af8f1bd06baa
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-09-23 18:10:00 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt () {
+    $('p').click(function Hey() {
+      alert ("Hey!")
+    }
+  )}
+
+function frameIt () {
+  $('img').load(function Tasty() {
+    $('img[src="https://s3.amazonaws.com/after-school-assets/minion-tongue.jpg"]').addClass("tasty");
+  }
+)}
+
+
+function pressIt () {
+  $('input#typing').keydown(function (key) {
+  if (key.which == 71) {
+    alert ("G was pressed");
+  }
+  }
+)}
+
+function submitIt () {
+$('form').submit(function SubmitForm () {
+  if ($("input:last").click()) {
+    alert ("Your form is going to be submitted now.");
+  }
+}
+)}
 
 $(document).ready(function(){
 
-// call functions here
 
 });
